26ed4db979a680c95bd8fc9186c8250369fcf92b
italolimad04 <italo.lima@urbis.cc>
2024-07-31 18:26:47 -0300

    debug: adding prints

--- cfs_q2.py
+++ cfs_q2.py
@@ -421,6 +421,7 @@ data_muito_relevante = data_aux[data_aux['Satisfação'] == 'Muito Relevante']
 
 # Calcular estatísticas descritivas
 def calcular_estatisticas(df, coluna):
+    print(df)
     df[coluna] = df[coluna].astype(float)
     print(df[coluna])
     estatisticas = df[coluna].describe(percentiles=[.25, .5, .75]).to_dict()
